55bafb404e5cd57ee3bcdd7cfaf8a9a00db24e97
TimEvci <Cimbom1993>
2020-11-07 22:18:33 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let slide2 = {
+    h1: "We are available all across the globe",
+    p: "With stores all over the world, it's easy for you to find furniture for your home or place of business.\n" +
+        "Locally, we’re in most major cities throughout the country. Find the branch nearest you using our\n" +
+        "store locator. Any questions? Don't hesitate to contact us today."
+}
+let slide3 = {
+    h1: "Manufactured with the best materials",
+    p: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology\n" +
+        "to ensure that every product is made as perfect and as consistent as possible. With three decades of\n" +
+        "experience in this industry, we understand what customers want for their home and office."
+}
+let slides = [slide2, slide3];
+
+
+
+
+
+
+
+
+
+
+
